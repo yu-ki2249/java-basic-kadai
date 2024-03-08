7e5bf1c5feb60b42fb7010703990127920d3edc3
yu-ki2249 <watabeyuki04092002@gmail.com>
2024-03-09 00:43:00 +0900

    kadai_017を追加

---
+++ kadai_017/KatoHanako_Chapter17.java
@@ -0,0 +1,12 @@
+package kadai_017;
+
+public class KatoHanako_Chapter17 extends Kato_Chapter17 {
+	//	名を表すフィールドをセットする
+	public void setGivenName() {
+		givenName = "花子";
+	}
+
+	public void eachIntroduce() {
+		System.out.println("趣味は読書です");
+	}
+}
